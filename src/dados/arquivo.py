259be21264a24b158aeeb2448e0aeb6b3764202b
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-10-15 21:43:48 -0300

    init

--- src/dados/arquivo.py
+++ src/dados/arquivo.py
@@ -1,5 +1,8 @@
 from src.dados.ioperacao_dados import IoperacaoDados
 import os
+import openpyxl
 
 
 class Arquivo(IoperacaoDados):
+    def __init__(self):
+        self._caminho_base = os.getcwd()
