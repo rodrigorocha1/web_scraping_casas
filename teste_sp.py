f1d99b1d3f126807125d7c2a3d7ad9b7e40392af
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-10-28 23:26:45 -0300

    Análise preço banheiros

--- teste_sp.py
+++ teste_sp.py
@@ -1,4 +1,22 @@
-def loop_teste():
-    a = [1, 2]
-    b = [2, 4]
-    return zip(a, b)
+df_temp = base_apartamento.query(f'bairro == "{bairro}"')
+print('=' * 40)
+print(f'Bairro: {bairro}')
+print(f'Média preço: {round(df_temp["precos"].mean(), 2)}')
+print(f'Moda preço: {round(df_temp["precos"].mode(), 2)}')
+print(f'Mediana preço: {round(df_temp["precos"].median(), 2)}')
+print(f'Desvio Padrão preço: {round(df_temp["precos"].std(), 2)}')
+print(f'Preço Máximo: {round(df_temp["precos"].max(), 2)}')
+print(f'Preço Minimo: {round(df_temp["precos"].min(), 2)}')
+for banheiro in df_temp['banheiros'].unique():
+
+    df_temp = df_temp.query(f'banheiros == {banheiro}')
+    if not df_temp.empty:
+        print(f'====Análise banheiro {banheiro}=========')
+        print(f'Média preço: {round(df_temp["precos"].mean(), 2)}')
+        print(f'Moda preço: {round(df_temp["precos"].mode(), 2)}')
+        print(f'Mediana preço: {round(df_temp["precos"].median(), 2)}')
+        print(f'Desvio Padrão preço: {round(df_temp["precos"].std(), 2)}')
+        print(f'Preço Máximo: {round(df_temp["precos"].max(), 2)}')
+        print(f'Preço Minimo: {round(df_temp["precos"].min(), 2)}')
+
+    print('=' * 40)
